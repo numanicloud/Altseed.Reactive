f2b74dc2ffd3e7e3c6335126ce2278aa95cbb9f2
NumAniCloud <num.ani.cloud@gmail.com>
2015-07-16 20:30:33 +0900

    Add Controller.

---
+++ Input/Controller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using asd;
+
+namespace Nac.Altseed.Input
+{
+	abstract class Controller<AbstractKey>
+	{
+		public abstract KeyState GetState(AbstractKey key);
+	}
+}
---
+++ Input/KeyboardController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using asd;
+
+namespace Nac.Altseed.Input
+{
+	class KeyboardController<AbstractKey> : Controller<AbstractKey>
+	{
+		private Dictionary<AbstractKey, Keys> binding;
+
+		public void BindKey(Keys key, AbstractKey abstractKey)
+		{
+			binding[abstractKey] = key;
+		}
+
+		public override KeyState GetState(AbstractKey key)
+		{
+			return Engine.Keyboard.GetKeyState(binding[key]);
+		}
+	}
+}
