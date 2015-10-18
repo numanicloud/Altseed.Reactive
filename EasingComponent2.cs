185193c27277c3d9ad2ca4c0a72b0f3f0f9d2f43
NumAniCloud <num.ani.cloud@gmail.com>
2015-10-18 20:25:35 +0900

    Add EasingComponent2.

---
+++ EasingComponent2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using asd;
+
+namespace Nac.Altseed
+{
+	public class EasingComponent2<TObject> : Object2DComponent where TObject : Object2D
+	{
+		int count, maxCount;
+		float lastValue;
+		float[] easing;
+		Func<TObject, float> getter;
+		Action<TObject, float> setter;
+		Action callback;
+
+		public EasingComponent2(
+			Func<TObject, float> getter,
+			Action<TObject, float> setter,
+			float goal,
+			EasingStart start,
+			EasingEnd end,
+			int count,
+			Action callback = null)
+		{
+			easing = Easing.GetEasingFunction(start, end);
+			lastValue = goal;
+			maxCount = count;
+
+			this.getter = getter;
+			this.setter = setter;
+			this.callback = callback;
+		}
+
+		protected override void OnUpdate()
+		{
+			var o = Owner as TObject;
+            ++count;
+			var v = Easing.GetNextValue(getter(o), lastValue, count, maxCount, easing);
+			setter(o, v);
+			if(count >= maxCount)
+			{
+				if(callback != null)
+				{
+					callback();
+				}
+				Vanish();
+			}
+		}
+
+		public static EasingComponent2<Object2D> CreateXEasing(float goal, EasingStart start, EasingEnd end, int count, Action callback = null)
+		{
+			return new EasingComponent2<Object2D>(
+				o => o.Position.X,
+				(o, v) => o.Position = new Vector2DF(v, o.Position.Y),
+				goal,
+				start,
+				end,
+				count,
+				callback);
+		}
+	}
+}
