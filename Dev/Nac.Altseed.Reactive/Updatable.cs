62a33cca834c80f2bb896f68dd6be1d9f8843be4
NumAniCloud <num.ani.cloud@gmail.com>
2015-11-24 14:53:56 +0900

    UpdatableなどにXMLコメントを追加

--- Dev/Nac.Altseed.Reactive/Updatable.cs
+++ Dev/Nac.Altseed.Reactive/Updatable.cs
@@ -9,14 +9,23 @@ using asd;
 
 namespace Nac.Altseed.Reactive
 {
+    /// <summary>
+    /// Updateされたことを通知するイベントを持つシングルトン クラス。
+    /// </summary>
     public class Updatable
     {
         private static Updatable instance_;
 
+        /// <summary>
+        /// このクラスのシングルトン オブジェクト。
+        /// </summary>
         public static Updatable Instance => instance_ ?? (instance_ = new Updatable());
 
         private Subject<float> frameUpdate;
 
+        /// <summary>
+        /// Updateが呼ばれたときに発行されるイベント。
+        /// </summary>
         public IObservable<float> FrameUpdate => frameUpdate;
 
         private Updatable()
@@ -24,6 +33,9 @@ namespace Nac.Altseed.Reactive
             frameUpdate = new Subject<float>();
         }
 
+        /// <summary>
+        /// FrameUpdateイベントを発行します。毎フレーム呼び出すことが推奨されます。
+        /// </summary>
         public void Update()
         {
             frameUpdate.OnNext(Engine.DeltaTime);
--- Dev/Nac.Altseed.Reactive/UpdatableSynchronizationContext.cs
+++ Dev/Nac.Altseed.Reactive/UpdatableSynchronizationContext.cs
@@ -6,6 +6,9 @@ namespace Nac.Altseed.Reactive
 {
     using TaskItem = Tuple<SendOrPostCallback, object>;
 
+    /// <summary>
+    /// 送られた継続処理を Update が呼ばれるたびに同一のスレッド上で実行する同期コンテキスト。
+    /// </summary>
     public class UpdatableSynchronizationContext : SynchronizationContext
 	{
 		ConcurrentQueue<TaskItem> queue;
@@ -15,6 +18,11 @@ namespace Nac.Altseed.Reactive
 			queue = new ConcurrentQueue<TaskItem>();
 		}
 
+        /// <summary>
+        /// この同期コンテキストに継続処理を送ります。
+        /// </summary>
+        /// <param name="d">継続処理。</param>
+        /// <param name="state"></param>
 		public override void Post(SendOrPostCallback d, object state)
 		{
 			if(d == null)
@@ -29,6 +37,9 @@ namespace Nac.Altseed.Reactive
 			throw new NotImplementedException();
 		}
 
+        /// <summary>
+        /// 送られてきた継続処理を実行します。アプリケーションの実行中に定期的に呼び出すことが推奨されます。
+        /// </summary>
 		public void Update()
 		{
 			TaskItem item;
